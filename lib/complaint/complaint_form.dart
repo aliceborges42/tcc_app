9fbd356e7d2812947d019a01b1436ae2074721da
aliceborges42 <aliceborges42.ab@gmail.com>
2024-03-13 10:46:32 -0300

    chore styles form

--- lib/complaint/complaint_form.dart
+++ lib/complaint/complaint_form.dart
@@ -10,6 +10,10 @@ import 'package:tcc_app/models/complaint_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
+import 'package:tcc_app/utils/colors.dart';
+import 'package:tcc_app/utils/global_variable.dart';
+import 'package:tcc_app/components/my_button.dart';
+import 'package:tcc_app/components/my_textfield.dart';
 
 class ComplaintForm extends StatefulWidget {
   const ComplaintForm({super.key});
@@ -23,6 +27,7 @@ class _ComplaintFormState extends State<ComplaintForm> {
   LatLng? _selectedLocation;
   final authuser = FirebaseAuth.instance.currentUser!;
   String complaintType = '';
+  bool _isLoading = false;
 
   final desordemItems = [
     'Poste de Luz Danificado',
@@ -37,24 +42,71 @@ class _ComplaintFormState extends State<ComplaintForm> {
     'Estupro'
   ];
 
+  void sendComplaint() async {
+    setState(() {
+      _isLoading = true;
+    });
+    if (_formKey.currentState!.saveAndValidate()) {
+      Map<String, dynamic> formData = _formKey.currentState!.value;
+      try {
+        GeoPoint? geoPoint;
+        if (_selectedLocation != null) {
+          geoPoint = GeoPoint(
+            _selectedLocation!.latitude,
+            _selectedLocation!.longitude,
+          );
+        }
+        final res = await FireStoreMethods().uploadPost(
+          formData['descricao'],
+          formData['images']!,
+          authuser.uid,
+          geoPoint,
+          formData['dataOcorrido'],
+          formData['horaOcorrido'],
+          formData['tipoDenuncia'],
+          formData['tipoEspecificacao'],
+        );
+        print('res: $res');
+      } catch (err) {
+        print(err);
+      }
+      setState(() {
+        _isLoading = false;
+      });
+      _formKey.currentState!.reset();
+      _selectedLocation = null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 15),
       child: FormBuilder(
         key: _formKey,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             FormBuilderTextField(
               name: 'descricao',
-              decoration: const InputDecoration(labelText: 'Descrição'),
+              decoration: myDecoration.copyWith(
+                hintText:
+                    "Descrição", // Atualizando o hintText com o texto fornecido
+              ),
               validator: FormBuilderValidators.compose([
                 FormBuilderValidators.required(),
               ]),
             ),
+            const SizedBox(
+              height: 12,
+            ),
             FormBuilderDropdown(
               name: 'tipoDenuncia',
-              decoration: InputDecoration(labelText: 'Tipo de Denúncia'),
+              // decoration: InputDecoration(labelText: 'Tipo de Denúncia'),
+              decoration: myDecoration.copyWith(
+                hintText:
+                    "Tipo de Denúncia", // Atualizando o hintText com o texto fornecido
+              ),
               // hint: Text('Selecione o tipo de denúncia'),
               validator: FormBuilderValidators.compose([
                 FormBuilderValidators.required(),
@@ -73,6 +125,9 @@ class _ComplaintFormState extends State<ComplaintForm> {
               },
             ),
             // Adicionar campo 2.1 (seleção condicional)
+            const SizedBox(
+              height: 12,
+            ),
             if (complaintType != '')
               FormBuilderDropdown(
                 name: 'tipoEspecificacao',
@@ -88,30 +143,62 @@ class _ComplaintFormState extends State<ComplaintForm> {
                         ))
                     .toList(),
               ),
-
+            const SizedBox(
+              height: 12,
+            ),
             FormBuilderDateTimePicker(
               name: 'dataOcorrido',
               inputType: InputType.date,
               format: DateFormat('dd/MM/yyyy'),
-              decoration: InputDecoration(labelText: 'Data do Ocorrido'),
+              decoration: myDecoration.copyWith(
+                hintText:
+                    "Data do Ocorrido", // Atualizando o hintText com o texto fornecido
+              ),
               validator: FormBuilderValidators.compose([
                 FormBuilderValidators.required(),
               ]),
+            ),
+            const SizedBox(
+              height: 12,
             ),
             FormBuilderDateTimePicker(
               name: 'horaOcorrido',
               inputType: InputType.time,
-              decoration: InputDecoration(labelText: 'Hora do Ocorrido'),
+              decoration: myDecoration.copyWith(
+                hintText:
+                    "Hora do Ocorrido", // Atualizando o hintText com o texto fornecido
+              ),
               validator: FormBuilderValidators.compose([
                 FormBuilderValidators.required(),
               ]),
             ),
+            const SizedBox(
+              height: 20,
+            ),
             FormBuilderImagePicker(
               name: 'images',
-              decoration: const InputDecoration(labelText: 'Imagens do Local'),
+              decoration: myDecoration.copyWith(
+                labelText:
+                    "Imagens do Local", // Atualizando o hintText com o texto fornecido
+              ),
               maxImages: 5,
             ),
+            const SizedBox(
+              height: 12,
+            ),
+
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor:
+                    Colors.white, // Define a cor do texto como preto
+                elevation: 0, // Define a elevação do botão
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                      10.0), // Define o raio do canto do botão
+                  side: BorderSide(color: lightBlack), // Define a cor da borda
+                ),
+              ),
               onPressed: () async {
                 // Abrir o diálogo do mapa
                 LatLng? selectedLocation = await showDialog(
@@ -135,38 +222,43 @@ class _ComplaintFormState extends State<ComplaintForm> {
             if (_selectedLocation != null)
               Text('Localização Selecionada: $_selectedLocation'),
 
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                if (_formKey.currentState!.saveAndValidate()) {
-                  Map<String, dynamic> formData = _formKey.currentState!.value;
-                  try {
-                    GeoPoint? geoPoint;
-                    if (_selectedLocation != null) {
-                      geoPoint = GeoPoint(
-                        _selectedLocation!.latitude,
-                        _selectedLocation!.longitude,
-                      );
-                    }
-                    final res = await FireStoreMethods().uploadPost(
-                      formData['descricao'],
-                      formData['images']!,
-                      authuser.uid,
-                      geoPoint,
-                      formData['dataOcorrido'],
-                      formData['horaOcorrido'],
-                      formData['tipoDenuncia'],
-                      formData['tipoEspecificacao'],
-                    );
-                    print('res: $res');
-                  } catch (err) {
-                    print(err);
-                  }
-                  _formKey.currentState!.reset();
-                }
-              },
-              child: Text('Enviar Denúncia'),
-            ),
+            SizedBox(height: 32),
+
+            MyButton(
+                onTap: sendComplaint,
+                buttonText: 'Enviar Denúncia',
+                isLoading: _isLoading)
+            // ElevatedButton(
+            //   onPressed: () async {
+            //     if (_formKey.currentState!.saveAndValidate()) {
+            //       Map<String, dynamic> formData = _formKey.currentState!.value;
+            //       try {
+            //         GeoPoint? geoPoint;
+            //         if (_selectedLocation != null) {
+            //           geoPoint = GeoPoint(
+            //             _selectedLocation!.latitude,
+            //             _selectedLocation!.longitude,
+            //           );
+            //         }
+            //         final res = await FireStoreMethods().uploadPost(
+            //           formData['descricao'],
+            //           formData['images']!,
+            //           authuser.uid,
+            //           geoPoint,
+            //           formData['dataOcorrido'],
+            //           formData['horaOcorrido'],
+            //           formData['tipoDenuncia'],
+            //           formData['tipoEspecificacao'],
+            //         );
+            //         print('res: $res');
+            //       } catch (err) {
+            //         print(err);
+            //       }
+            //       _formKey.currentState!.reset();
+            //     }
+            //   },
+            //   child: Text('Enviar Denúncia'),
+            // ),
           ],
         ),
       ),
